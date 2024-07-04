40b2c5fb983fde2a26f253ed6c6c07207445e1e8
Dandi2k8 <drd.construction30@gmail.com>
2024-07-03 19:36:30 -0700

    Adding the rest of the planet paintings

--- src/main/java/net/teddy0008/ad_extendra/painting/ModPaintings.java
+++ src/main/java/net/teddy0008/ad_extendra/painting/ModPaintings.java
@@ -11,6 +11,30 @@ public class ModPaintings {
     public static final RegistryEntry<PaintingVariant> CERES = PAINTING_VARIANTS.register("ceres", () -> {
         return new PaintingVariant(16, 16);
     });
+    public static final RegistryEntry<PaintingVariant> ORCUS = PAINTING_VARIANTS.register("orcus", () -> {
+        return new PaintingVariant(16, 16);
+    });
+    public static final RegistryEntry<PaintingVariant> HAUMEA = PAINTING_VARIANTS.register("haumea", () -> {
+        return new PaintingVariant(16, 16);
+    });
+    public static final RegistryEntry<PaintingVariant> QUAOAR = PAINTING_VARIANTS.register("quaoar", () -> {
+        return new PaintingVariant(16, 16);
+    });
+    public static final RegistryEntry<PaintingVariant> MAKEMAKE = PAINTING_VARIANTS.register("makemake", () -> {
+        return new PaintingVariant(16, 16);
+    });
+    public static final RegistryEntry<PaintingVariant> GONGGONG = PAINTING_VARIANTS.register("gonggong", () -> {
+        return new PaintingVariant(16, 16);
+    });
+    public static final RegistryEntry<PaintingVariant> ERIS = PAINTING_VARIANTS.register("eris", () -> {
+        return new PaintingVariant(16, 16);
+    });
+    public static final RegistryEntry<PaintingVariant> SEDNA = PAINTING_VARIANTS.register("sedna", () -> {
+        return new PaintingVariant(16, 16);
+    });
+    public static final RegistryEntry<PaintingVariant> B = PAINTING_VARIANTS.register("b", () -> {
+        return new PaintingVariant(32, 32);
+    });
 
     public ModPaintings() {
     }
