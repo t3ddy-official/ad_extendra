49541123757bd8a6b0e42c2c4e822d9ea144d797
Dandi2k8 <drd.construction30@gmail.com>
2024-07-30 14:25:34 -0700

    Adding the mushroom loot tables

--- src/main/java/net/teddy0008/ad_extendra/datagen/ModGlobalLootModifiersGenerator.java
+++ src/main/java/net/teddy0008/ad_extendra/datagen/ModGlobalLootModifiersGenerator.java
@@ -1,10 +1,13 @@
 package net.teddy0008.ad_extendra.datagen;
 
+import earth.terrarium.ad_astra.AdAstra;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
 import net.minecraftforge.common.data.GlobalLootModifierProvider;
+import net.minecraftforge.common.loot.LootTableIdCondition;
 import net.teddy0008.ad_extendra.Main;
 import net.teddy0008.ad_extendra.block.ModBlocks;
 import net.teddy0008.ad_extendra.loot.AddItemModifier;
@@ -27,5 +30,21 @@ public class ModGlobalLootModifiersGenerator extends GlobalLootModifierProvider 
         add("strophar_mushroom_from_cap", new AddItemModifier(new LootItemCondition[] {
                 LootItemBlockStatePropertyCondition.hasBlockStateProperties(earth.terrarium.ad_astra.common.registry.ModBlocks.STROPHAR_CAP.get()).build(),
                 LootItemRandomChanceCondition.randomChance(0.35f).build()}, ModBlocks.STROPHAR_MUSHROOM.get().asItem()));
+
+        add("chests/village/moon/house/aeronos_mushroom", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation(AdAstra.MOD_ID, "chests/village/moon/house")).build(),
+        LootItemRandomChanceCondition.randomChance(0.75f).build()}, ModBlocks.AERONOS_MUSHROOM.get().asItem()));
+
+        add("chests/village/moon/house/strophar_mushroom", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation(AdAstra.MOD_ID, "chests/village/moon/house")).build(),
+                LootItemRandomChanceCondition.randomChance(0.625f).build()}, ModBlocks.STROPHAR_MUSHROOM.get().asItem()));
+
+        add("chests/village/moon/blacksmith/aeronos_mushroom", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation(AdAstra.MOD_ID, "chests/village/moon/blacksmith")).build(),
+                LootItemRandomChanceCondition.randomChance(0.75f).build()}, ModBlocks.AERONOS_MUSHROOM.get().asItem()));
+
+        add("chests/village/moon/blacksmith/strophar_mushroom", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation(AdAstra.MOD_ID, "chests/village/moon/blacksmith")).build(),
+                LootItemRandomChanceCondition.randomChance(0.625f).build()}, ModBlocks.STROPHAR_MUSHROOM.get().asItem()));
     }
 }
